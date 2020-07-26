4a7608391845f1353e73ee2d9a6dbbe9dbbeeb5e
aizf <45379838+aizf@users.noreply.github.com>
2020-07-26 12:46:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+notes='./source/_posts/notes/'
 index_deploy='../index-deploy/www/'
 ignores=('projects' 'projects1')
 dist='./dist/'
@@ -9,16 +10,16 @@ dist='./dist/'
 set -e
 
 
-# 生成静态文件
-npm run clean
-npm run build
+cd $notes
+git add -A && git commit -m '+' && git push
+cd -
 
 
-# 压缩
-npx gulp
+# 生成静态文件,压缩
+npm run clean && npm run build && npx gulp
 
 
-mkdir -p $index_deploy
+test -e $index_deploy || mkdir -p $index_deploy
 
 # 删除旧文件
 cd $index_deploy
@@ -29,28 +30,22 @@ do
         echo "$(realpath $var) retained"
         continue
     else
-        echo "$(realpath $var) deleted"
-        rm -rf $var
+        rm -rf $var && echo "$(realpath $var) deleted"
     fi
 done
 cd -
 
 
 # dist不存在则报错
-if !(test -e $dist);then
-    exit 1
-fi
-
+test -e $dist || exit 1
 
 # 复制新文件
 cp -af "$dist." $index_deploy
 
 
 cd "$index_deploy/.."
-git add -A
-git commit -m 'deploy'
-git push
-
-
+git add -A && git commit -m 'deploy' && git push
 cd -
+
+
 echo "Completed !"
