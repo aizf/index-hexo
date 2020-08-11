4325bfc32937948b28ed0788020e6780e101b775
aizf <45379838+aizf@users.noreply.github.com>
2020-08-11 10:14:41 +0800

    更改流程

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,17 @@
 notes='./source/_posts/notes/'
 index_deploy='../index-deploy/'
 index_deploy_index="${index_deploy}www/index/"
-ignores=('projects' 'projects1')
 dist='./dist/'
 
 
 # 确保脚本抛出遇到的错误
 set -e
 
+realpath .
+
 test -d $index_deploy || exit 1
 
-
-cd $notes
-git add -A && git commit -m '+' && git push
-cd -
-
+git submodule update --init --remote --recursive
 
 # 生成静态文件,压缩
 npm run clean && npm run build && npx gulp
